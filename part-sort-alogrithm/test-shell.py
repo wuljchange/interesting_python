647c545758a3fec5fed7a7f487382323ffe5635d
WULINJIANG1 <wulinjiang1@kingsoft.com>
2019-04-29 10:33:03 +0800

    [feat] 希尔排序添加注释

--- part-sort-alogrithm/test-shell.py
+++ part-sort-alogrithm/test-shell.py
@@ -1,10 +1,12 @@
 # 希尔排序 时间复杂度是O(NlogN)
-# 又称缩小增量排序
+# 又称缩小增量排序 首先设置一个基础增量d，对每间隔d的元素分组，然后对每个分组的元素进行直接插入排序
+# 然后缩小增量，用同样的方法，直到增量小于0时，排序完成
 
 
 def shell_sort(data: list):
     n = len(data)
-    gap = int(n / 2)
+    gap = int(n / 2)  # 设置基础增量
+    # 当增量小于0时，排序完成
     while gap > 0:
         for i in range(gap, n):
             j = i
